aeca32fcf39623bb3b73ad90e7b44e70ba470b10
Mahdi <mahdibagherichanel@gmail.com>
2025-02-21 20:13:53 +0330

    Capability of deleting the item in the cabin

--- src/services/apicabins.js
+++ src/services/apicabins.js
@@ -7,3 +7,11 @@ export async function getCabins() {
   }
   return data;
 }
+
+export async function deletecabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+  if (error) {
+    console.log("cabins could not delet");
+  }
+  return data;
+}
