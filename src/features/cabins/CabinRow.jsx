cf139b27180a0ef3c021d744a997add36b8d02a9
Mahdi <mahdibagherichanel@gmail.com>
2025-02-28 23:14:47 +0330

    Fix typo in useDeleteCabin hook and implement new hook for cabin deletion; remove unused outside click handler.

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 import CreateCabinForm from "./CreateCabinForm";
-import { useDeleateCabin } from "./useDeletCabin";
+import { useDeleteCabin } from "./useDeleteCabin";
 import { formatCurrency } from "../../utils/helpers";
 import { HiPencil, HiSquare2Stack, HiTrash } from "react-icons/hi2";
 import useCreateCabin from "./useCreateCabin";
@@ -50,7 +50,7 @@ const Discount = styled.div`
 `;
 
 function CabinRow({ cabin }) {
-  const { isDeleting, deleteCabin } = useDeleateCabin();
+  const { isDeleting, deleteCabin } = useDeleteCabin();
   const { createCabin } = useCreateCabin();
 
   const {
--- src/features/cabins/useDeleteCabin.js
+++ src/features/cabins/useDeleteCabin.js
@@ -2,7 +2,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { deletecabin as deleteApi } from "../../services/apicabins";
 
-export function useDeleateCabin() {
+export function useDeleteCabin() {
   const queryClient = useQueryClient();
 
   const { isLoading: isdeleteing, mutate: deletecabin } = useMutation({
